1904c138dc7ace0ed9357aa90569f348d946385d
Nicolas reveco <nreveco.rs@gmail.com>
2023-07-11 19:59:49 -0400

    Add code laravel crud example

---
+++ resources/js/components/Product.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h5 id="associating-form-text-with-form-controls">Nombre:</h5>
+        <h6>{{ product.name }}</h6>
+        <h5 id="associating-form-text-with-form-controls">Descripción:</h5>
+        <p>{{ product.description }}</p>
+        <h5 id="associating-form-text-with-form-controls">Precio:</h5>
+        <p>Precio: {{ product.price }}</p>
+        <router-link :to="`/products/${product.id}/edit`" class="btn btn-primary">Editar</router-link>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+data() {
+    return {
+    product: {}
+    }
+},
+async created() {
+    try {
+    const response = await axios.get(`/api/products/${this.$route.params.id}`);
+    this.product = response.data;
+    } catch (error) {
+    console.error(error);
+    }
+}
+}
+</script>
